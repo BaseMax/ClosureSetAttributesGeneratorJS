b2999d55d3999ea612d6c9d0d7816f698f392488
Max Base <MaxBaseCode@Gmail.Com>
2022-12-10 09:51:25 +0100

    Update ClosureSetAttributesGenerator.js

--- ClosureSetAttributesGenerator.js
+++ ClosureSetAttributesGenerator.js
@@ -78,52 +78,53 @@ const finds = (F, Z) => {
 };
 
 //// Test Caswes
+let out;
 
-// let out = finds([
-//     {from: ["A"], to: ["B"]},
-//     {from: ["B"], to: ["D"]},
-//     {from: ["A"], to: ["K"]},
-//     {from: ["K"], to: ["C"]},
-// ], ["A"]);
+out = finds([
+    {from: ["A"], to: ["B"]},
+    {from: ["B"], to: ["D"]},
+    {from: ["A"], to: ["K"]},
+    {from: ["K"], to: ["C"]},
+], ["A"]);
 
-// let out = finds([
-//     {from: ["T"], to: ["U"]},
-//     {from: ["V"], to: ["S", "W"]},
-//     {from: ["S"], to: ["T"]},
-// ], ["S", "V"]);
+out = finds([
+    {from: ["T"], to: ["U"]},
+    {from: ["V"], to: ["S", "W"]},
+    {from: ["S"], to: ["T"]},
+], ["S", "V"]);
 // S, V, W, T, U
 
-// let out = finds([
-//     {from: ["T"], to: ["U"]},
-//     {from: ["V"], to: ["S", "W"]},
-//     {from: ["S"], to: ["T"]},
-// ], ["V"]);
+out = finds([
+    {from: ["T"], to: ["U"]},
+    {from: ["V"], to: ["S", "W"]},
+    {from: ["S"], to: ["T"]},
+], ["V"]);
 // V, S, W, T, U
 
-// let out = finds([
-//     {from: ["T"], to: ["U"]},
-//     {from: ["V"], to: ["S", "W"]},
-//     {from: ["S"], to: ["T"]},
-// ], ["S"]);
+out = finds([
+    {from: ["T"], to: ["U"]},
+    {from: ["V"], to: ["S", "W"]},
+    {from: ["S"], to: ["T"]},
+], ["S"]);
 // S, T, U
 
-// let out = finds([
-//     {from: ["U"], to: ["V", "X", "Q"]},
-//     {from: ["U", "V", "P"], to: ["O"]},
-//     {from: ["O", "Q"], to: ["Y", "Z"]},
-//     {from: ["U", "P"], to: ["X", "Y"]},
-// ], ["U", "P"]);
+out = finds([
+    {from: ["U"], to: ["V", "X", "Q"]},
+    {from: ["U", "V", "P"], to: ["O"]},
+    {from: ["O", "Q"], to: ["Y", "Z"]},
+    {from: ["U", "P"], to: ["X", "Y"]},
+], ["U", "P"]);
 // U, P, V, X, Q, O, Y, Z
 
-// let out = finds([
-//     {from: ["U"], to: ["V", "X", "Q"]},
-//     {from: ["U", "V", "P"], to: ["O"]},
-//     {from: ["O", "Q"], to: ["Y", "Z"]},
-//     {from: ["U", "P"], to: ["X", "Y"]},
-// ], ["O", "Q"]);
+out = finds([
+    {from: ["U"], to: ["V", "X", "Q"]},
+    {from: ["U", "V", "P"], to: ["O"]},
+    {from: ["O", "Q"], to: ["Y", "Z"]},
+    {from: ["U", "P"], to: ["X", "Y"]},
+], ["O", "Q"]);
 // O, Q, Y, Z
 
-let out = finds([
+out = finds([
     {from: ["U"], to: ["V", "X", "Q"]},
     {from: ["U", "V", "P"], to: ["O"]},
     {from: ["O", "Q"], to: ["Y", "Z"]},
